7a448179aba24e8caecf6108616d062545de3c39
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-09-06 23:47:57 +0700

    style(*): update homepage style

--- app/page.tsx
+++ app/page.tsx
@@ -15,37 +15,46 @@ export default async function Home() {
   })
   const session = await getServerSession(authOptions)
 
+  let numCols = 1
+  if (posts.length > 1) {
+    numCols = posts.length >= 3 ? 3 : 2
+  }
+
   return (
-    <div className="bg-gray-900 min-h-screen flex flex-col items-center justify-center text-white">
-      <header className="text-center mb-8">
-        <h1 className="text-5xl font-extrabold text-indigo-700">
-          Welcome to Next.js + Prisma Blog
-        </h1>
+    <div className="bg-gray-900 min-h-screen text-white">
+      <header>
+        <div className="bg-indigo-900 py-4 text-center">
+          <div className="flex justify-between items-center p-4">
+            <h1 className="text-5xl font-extrabold text-indigo-100">
+              Welcome to Next.js + Prisma Blog
+            </h1>
+            <div className="flex items-center gap-4">
+              {session ? <LogoutButton /> : <LoginButton />}
+              {session ? <ProfileButton /> : <RegisterButton />}
+            </div>
+          </div>
+        </div>
       </header>
-      <main className="max-w-screen-lg mt-8 w-full grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+      <main
+        className={`max-w-screen-xl mx-auto mt-8 w-full grid grid-cols-1 gap-6 md:grid-cols-${numCols}`}
+      >
         {posts.map(post => (
           <article
             key={post.id}
-            className="bg-gray-800 rounded-lg shadow-md overflow-hidden transform hover:scale-105 hover:shadow-2xl transition-transform duration-300 ease-in-out"
+            className="bg-gray-800 rounded-lg shadow-lg overflow-hidden transform hover:scale-105 hover:shadow-2xl transition-transform duration-300 ease-in-out"
           >
             <div className="p-6">
-              <h2 className="text-xl font-semibold text-indigo-700">
+              <h2 className="text-xl font-semibold text-indigo-300">
                 {post.title}
               </h2>
               <p className="mt-2 text-gray-400">{post.content}</p>
             </div>
             <div className="p-4 bg-gray-700">
-              <p className="text-sm text-indigo-400">By {post.author?.name}</p>
+              <p className="text-sm text-indigo-300">By {post.author?.name}</p>
             </div>
           </article>
         ))}
       </main>
-      <div className="mt-16 flex space-x-4">
-        <LoginButton />
-        <RegisterButton />
-        <LogoutButton />
-        <ProfileButton />
-      </div>
     </div>
   )
 }
