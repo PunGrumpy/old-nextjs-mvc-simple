6c69adf4f004e11718009f84e875ac988507ff1f
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-09-06 21:58:18 +0700

    fix(api): update true path

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,4 +1,4 @@
-import prisma from '@/app/lib/prisma'
+import prisma from '@/app/libs/prisma'
 import { NextRequest, NextResponse } from 'next/server'
 
 // GET /api/posts
--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import prisma from '@/app/lib/prisma'
+import prisma from '@/app/libs/prisma'
 
 // GET /api/users
 export async function GET() {
